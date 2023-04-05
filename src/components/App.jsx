89e66564290803c8b00221265cae8fb3c7886ac0
Artem Karpenko <dante120591@gmail.com>
2023-04-05 20:59:29 +0300

    Basic navigation improved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import { RegisterPage } from 'pages/RegisterPage';
 import { LoginPage } from 'pages/LoginPage';
 import { DashboardPage } from 'pages/DashboardPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
+import { WelcomePage } from 'pages/WelcomePage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<WelcomePage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="dashboard" element={<DashboardPage />} />
---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,9 @@
+import { Welcome } from 'components/Welcome';
+
+export const WelcomePage = () => {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+};
